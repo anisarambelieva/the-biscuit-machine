7180e3820db53b6cc675235cc781010940be163b
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2021-12-15 14:15:25 +0200

    Edit App.js test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders The Biscuit Machine heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/The Biscuit Machine/i);
+  expect(headingElement).toBeInTheDocument();
 });
