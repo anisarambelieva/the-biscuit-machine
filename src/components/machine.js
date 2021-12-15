548c828056f9c7e2411b2c618d98e3a7e635362f
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2021-12-15 17:03:35 +0200

    Add machine

---
+++ src/components/machine.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import Conveyor from "./conveyor";
+import Oven from "./oven";
+import Switch from "./switch";
+
+const Machine = () => {
+  const [switchState, setSwitchState] = useState("OFF");
+  const [ovenDegrees, setOvenDegrees] = useState(0);
+  const [conveyorWorking, setConveyorWorking] = useState(false);
+
+  // if the switch is turned on, preheat oven to 240 degrees and then stop
+  // if the switch is off, start decreasing the degrees
+  useEffect(() => {
+    if (switchState === "ON" && ovenDegrees < 240) {
+      setTimeout(() => {
+        setOvenDegrees(ovenDegrees + 1);
+      }, 100);
+    } else if (switchState === "OFF" && ovenDegrees > 0) {
+      setTimeout(() => {
+        setOvenDegrees(ovenDegrees - 1);
+      }, 100);
+    }
+
+    if(ovenDegrees >= 220 && !conveyorWorking) setConveyorWorking(true);
+
+  }, [switchState, ovenDegrees, conveyorWorking]);
+
+  return (
+    <>
+      <Oven ovenDegrees={ovenDegrees}></Oven>
+      <Switch onChange={setSwitchState} />
+      <Button onClick={() => console.log(switchState)}>Check state</Button>
+      <Button onClick={() => console.log(ovenDegrees)}>
+        Check oven degrees
+      </Button>
+      <Conveyor conveyorWorking={conveyorWorking.toString()}></Conveyor>
+    </>
+  );
+};
+
+export default Machine;
