39500388b90dc3503de2f2dc42b843f1bf5f10fe
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2021-12-24 12:16:01 +0200

    Add padding to buttons

--- src/components/switch.js
+++ src/components/switch.js
@@ -2,11 +2,11 @@ import { Button } from "react-bootstrap";
 
 const Switch = () => {
   return (
-    <div>
+    <div style={{padding: '5px'}}>
       <p>Switch</p>
-      <Button style={{padding: 0, marginRight: '5px'}}>On</Button>
-      <Button style={{padding: 0,  marginRight: '5px'}}>Paused</Button>
-      <Button style={{padding: 0}}>Off</Button>
+      <Button style={{padding: '0 3px', marginRight: '5px'}}>On</Button>
+      <Button style={{padding: '0 3px',  marginRight: '5px'}}>Paused</Button>
+      <Button style={{padding: '0 3px'}}>Off</Button>
     </div>
   );
 };
