231e5a6408d73396ec20d5edb9dce97feb3ec1c9
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2021-12-29 14:57:20 +0200

    Make stamped and baked biscuits appear on time

--- src/components/biscuit/biscuitLifecycle.js
+++ src/components/biscuit/biscuitLifecycle.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { Col } from "react-bootstrap";
 import BakedBiscuit from "./bakedBiscuit";
 import Dough from "./dough";
 import Biscuit from "./rawBiscuit";
 
 const BiscuitLifecycle = () => {
+  const [stamped, setStamped] = useState(false);
+  const [baked, setBaked] = useState(false);
+
+  setTimeout(() => {
+    setStamped(true);
+  }, 6500);
+
+  setTimeout(() => {
+    setBaked(true);
+  }, 11500);
+
   return (
     <>
       <Col md="1">
@@ -11,11 +23,11 @@ const BiscuitLifecycle = () => {
       </Col>
 
       <Col md="1">
-        <Biscuit />
+        {stamped && <Biscuit />}
       </Col>
 
       <Col md={{ span: 1, offset: 2 }}>
-        <BakedBiscuit />
+        {baked && <BakedBiscuit />}
       </Col>
     </>
   );
