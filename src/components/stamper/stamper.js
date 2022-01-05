3dd654a7f81995544d132787207aab55cba9be94
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-05 11:07:24 +0200

    Change className of Stamper to attach or detach animations

--- src/components/stamper/stamper.js
+++ src/components/stamper/stamper.js
@@ -1,18 +1,19 @@
 import "./stamper.css";
 
-const Stamper = () => (
-  <div
-    className="stamper"
-    style={{
-      backgroundColor: "lightgray",
-      position: "absolute",
-      textAlign: "center",
-      height: "50px",
-      width: "100px"
-    }}
-  >
-    Stamper
-  </div>
-);
-
+const Stamper = ({ stamp }) => {
+  return (
+    <div
+      className={stamp ? "stamper" : ""}
+      style={{
+        backgroundColor: "lightgray",
+        position: "absolute",
+        textAlign: "center",
+        height: "50px",
+        width: "100px",
+      }}
+    >
+      Stamper
+    </div>
+  );
+};
 export default Stamper;
