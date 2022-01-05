fae273a57beb4a090d7dd215e894729e4879bac4
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-05 15:22:33 +0200

    Shorten oven text to stop component from resizing on value change

--- src/components/oven.js
+++ src/components/oven.js
@@ -1,6 +1,6 @@
 const Oven = ({ ovenDegrees, heatingElement }) => (
   <div style={{ backgroundColor: "#ebbb86", padding: "5px" }}>
-    <p style={{ margin: 0 }}>Heating element is on: {heatingElement}</p>
+    <p style={{ margin: 0 }}>Heating element on: {heatingElement}</p>
     <p style={{ margin: 0 }}>Oven degrees: {ovenDegrees}</p>
   </div>
 );
