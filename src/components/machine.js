0dafb16f17f6c5fbb0bb9027ef9ed0a32d66a0b2
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-05 11:58:54 +0200

    Add animation play state to machine

--- src/components/machine.js
+++ src/components/machine.js
@@ -12,6 +12,7 @@ const Machine = () => {
   const [heatingElement, setHeatingElement] = useState(false);
   const [conveyorWorking, setConveyorWorking] = useState(false);
   const [stamping, setStamping] = useState(false);
+  const [animationState, setAnimationState] = useState("running");
 
   useEffect(() => {
     if (heatingElement) {
@@ -46,7 +47,7 @@ const Machine = () => {
         </Col>
 
         <Col md="1" style={{ position: "relative" }}>
-          <Stamper state={state} stamp={stamping} />
+          <Stamper animationState={animationState} stamp={stamping} />
         </Col>
 
         <Col
@@ -61,7 +62,7 @@ const Machine = () => {
       </Row>
 
       <Row style={{ position: "relative" }}>
-        {conveyorWorking && <BiscuitLifecycle state={state} onStamp={setStamping} />}
+        {conveyorWorking && <BiscuitLifecycle animationState={animationState} onStamp={setStamping} />}
       </Row>
 
       <Row style={{ height: "75px" }}></Row>
