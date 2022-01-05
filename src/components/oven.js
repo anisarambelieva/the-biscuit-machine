6dc14f5a9b32cdca62b8530360f4271be237bbcc
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-05 15:21:41 +0200

    Add color and padding to oven component

--- src/components/oven.js
+++ src/components/oven.js
@@ -1,8 +1,8 @@
 const Oven = ({ ovenDegrees, heatingElement }) => (
-  <>
+  <div style={{ backgroundColor: "#ebbb86", padding: "5px" }}>
     <p style={{ margin: 0 }}>Heating element is on: {heatingElement}</p>
     <p style={{ margin: 0 }}>Oven degrees: {ovenDegrees}</p>
-  </>
+  </div>
 );
 
 export default Oven;
