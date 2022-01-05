d29b2da6740bcdae172f63497d2820f5a2cd4138
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-05 11:06:13 +0200

    Manage stamping depending on Dough animations

--- src/components/biscuit/dough.js
+++ src/components/biscuit/dough.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "./styles/dough.css";
 
-const Dough = ({ onAnimationEnd, state }) => {
+const Dough = ({ onAnimationEnd, state, onStamp }) => {
   const [animationsCount, setAnimationsCount] = useState(1);
 
   const handleAnimationEnd = () => {
     setAnimationsCount(animationsCount + 1);
 
-    if (animationsCount % 3 === 0) {
+    if (animationsCount === 4) {
       onAnimationEnd(true);
+      
+      setTimeout(() => {
+        onStamp(false);
+      }, 1000);
+    } else if (animationsCount === 2) {
+      onStamp(true);
     }
   };
 
