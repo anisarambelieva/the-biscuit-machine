8cebd5893274fb10ead0d218e2fbbb8d2b456e4c
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-10 15:25:42 +0200

    Manage motor working state

--- src/components/motor.js
+++ src/components/motor.js
@@ -1,7 +1,17 @@
-const Motor = ({ conveyorWorking }) => (
-  <p style={{ backgroundColor: "lightblue", padding: "5px" }}>
-    Motor: {conveyorWorking ? "working" : "not working"}
-  </p>
-);
+import { useEffect, useState } from "react";
+
+const Motor = ({ conveyorWorking, machineState }) => {
+  const [working, setWorking] = useState(false);
+
+  useEffect(() => {
+    if (conveyorWorking) setWorking(true);
+  }, [conveyorWorking]);
+
+  return (
+    <p style={{ backgroundColor: "lightblue", padding: "5px" }}>
+      Motor: {working.toString()}
+    </p>
+  );
+}
 
 export default Motor;
