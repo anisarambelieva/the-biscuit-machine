44a41ca36c481b8dc7969213e09036fbdbe9addd
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-12 10:02:26 +0200

    Add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    extends: 'react-app',
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      'arrow-body-style': [
+        'error',
+        'as-needed',
+        { requireReturnForObjectLiteral: true },
+      ],
+      'react/no-array-index-key': 'error',
+      'import/extensions': ['error', 'always'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc' },
+        },
+      ],
+    },
+  };
+  
