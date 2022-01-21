621fda46f9f5428f50c6cadd09658b5576625485
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-01-21 14:53:38 +0200

    Add border radius to Container component

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,13 @@
 const BiscuitContainer = () => (
-  <p style={{ backgroundColor: "lightgray", padding: "5px" }}>Container</p>
+  <p
+    style={{
+      backgroundColor: "lightgray",
+      padding: "5px",
+      borderRadius: "5px",
+    }}
+  >
+    Container
+  </p>
 );
 
 export default BiscuitContainer;
