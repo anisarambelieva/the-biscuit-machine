569abbe164488c303ca752aac9950ab9f07cab7b
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-03 16:39:59 +0200

    Remove css in js from Stamper

--- src/components/stamper/stamper.js
+++ src/components/stamper/stamper.js
@@ -4,14 +4,7 @@ const Stamper = ({ stamp, animationState }) => (
   <div
     className={stamp ? "stamper" : ""}
     style={{
-      backgroundColor: "#DCD2CC",
-      position: "absolute",
-      textAlign: "center",
-      height: "50px",
-      width: "100px",
       WebkitAnimationPlayState: animationState,
-      zIndex: "1",
-      borderRadius: "5px",
     }}
   >
     Stamper
