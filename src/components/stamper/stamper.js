5efd66d30d02be0ec573dfd1a1f4d18ba66ee082
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-02-03 17:20:13 +0200

    Move Stamper CSS in JS to the separate CSS file (#62)

--- src/components/stamper/stamper.js
+++ src/components/stamper/stamper.js
@@ -2,16 +2,9 @@ import "./stamper.css";
 
 const Stamper = ({ stamp, animationState }) => (
   <div
-    className={stamp ? "stamper" : ""}
+    className={stamp ? "stamper animatedStamper" : "stamper"}
     style={{
-      backgroundColor: "#DCD2CC",
-      position: "absolute",
-      textAlign: "center",
-      height: "50px",
-      width: "100px",
       WebkitAnimationPlayState: animationState,
-      zIndex: "1",
-      borderRadius: "5px",
     }}
   >
     Stamper
