b1ec22ad014ee53e326648f67c4e07b0e5dfa196
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-09 16:07:17 +0200

    Move Raw Biscuit files to separate folder

--- src/components/Biscuit/RawBiscuit/index.test.js
+++ src/components/Biscuit/RawBiscuit/index.test.js
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 
-import Biscuit from "../rawBiscuit.js";
+import Biscuit from "./index.js";
 
 describe("<Biscuit />", () => {
   it("should match snapshot", () => {
