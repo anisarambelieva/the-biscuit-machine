31d632853780d59e04c103f86fcb7b6ddcf8a3b2
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-02-10 11:36:48 +0200

    Extract Dough hook (#81)

--- src/components/Biscuit/Dough/index.js
+++ src/components/Biscuit/Dough/index.js
@@ -1,40 +1,13 @@
-import { useState } from "react";
 import "./style.css";
 
-import * as constants from "../../../constants.js";
+import useDough from "./hook.js";
 
 const Dough = ({ onAnimationEnd, state, onStamp, machineState }) => {
-  const [animationsCount, setAnimationsCount] = useState(1);
-  const [className, setClassName] = useState("dough animatedDough");
-
-  const handleAnimationEnd = () => {
-    setAnimationsCount(animationsCount + 1);
-    let timer1, timer2;
-
-    if (animationsCount === 4) {
-      if (machineState !== constants.machineState.off) {
-        setClassName("dough");
-
-        timer1 = setTimeout(() => {
-          setClassName("dough animatedDough");
-          setAnimationsCount(1);
-        }, 100);
-      }
-
-      onAnimationEnd(true);
-
-      timer2 = setTimeout(() => {
-        onStamp(false);
-      }, 1000);
-    } else if (animationsCount === 2) {
-      onStamp(true);
-    }
-
-    return () => {
-      clearTimeout(timer1);
-      clearTimeout(timer2);
-    };
-  };
+  const { handleAnimationEnd, className } = useDough(
+    onAnimationEnd,
+    onStamp,
+    machineState
+  );
 
   return (
     <div
