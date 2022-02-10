042d32b2a1b702b3f58270e7b4a2d2bf26e46f4b
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-10 13:53:37 +0200

    Move hook to a separate file

--- src/components/Motor/index.js
+++ src/components/Motor/index.js
@@ -1,46 +1,5 @@
-import { useEffect, useState } from "react";
-
 import "./style.css";
-import * as constants from "../../constants.js";
-
-const useMotor = (
-  machineState,
-  setMotorWorking,
-  motorWorking,
-  setConveyorWorking
-) => {
-  const [motorLabel, setMotorLabel] = useState(constants.motorState.off);
-
-  useEffect(() => {
-    if (machineState === constants.machineState.paused) {
-      setMotorLabel(constants.motorState.paused);
-    } else if (motorWorking) {
-      setMotorLabel(constants.motorState.working);
-    } else {
-      setMotorLabel(constants.motorState.off);
-    }
-  }, [machineState, motorWorking]);
-
-  useEffect(() => {
-    setConveyorWorking(motorWorking);
-  }, [motorWorking, setConveyorWorking]);
-
-  useEffect(() => {
-    let timer;
-
-    if (motorWorking && machineState === constants.machineState.off) {
-      timer = setTimeout(() => {
-        setMotorWorking(false);
-      }, 17000);
-    }
-
-    return () => clearTimeout(timer);
-  }, [machineState, motorWorking, setMotorWorking]);
-
-  return {
-    motorLabel,
-  };
-};
+import useMotor from "./hook.js";
 
 const Motor = ({
   machineState,
